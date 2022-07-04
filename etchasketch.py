c4777532b76c23b44c9b3a4da03a222aa8ac6f33
Iroh02 <106004868+Iroh02@users.noreply.github.com>
2022-07-04 20:28:37 +0400

    Add files via upload

---
+++ etchasketch.py
@@ -0,0 +1,32 @@
+from turtle import Turtle,Screen
+tim = Turtle()
+screen=Screen()
+
+def moveforwards():
+    tim.forward(10)
+def movebackwards():
+    tim.backward(10)
+
+def moveright():
+  tim.right(10)
+def moveleft():
+    tim.left(10)
+def cleart():
+    tim.reset()
+
+screen.listen()
+screen.onkey(key="w",fun=moveforwards)
+screen.onkey(movebackwards,"s")
+screen.onkey(moveleft,"a")
+screen.onkey(moveright,"d")
+screen.onkey(cleart,"c")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
